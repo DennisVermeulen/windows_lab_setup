984172ac3c6ac170bf3c056b36d4afce01ea13ce
Dennis Vermeulen <d.a.m.vermeulen@gmail.com>
2022-05-10 14:15:35 +0200

    main

---
+++ scripts/installs.ps1
@@ -0,0 +1,167 @@
+param(
+    [Parameter(Mandatory = $False)]
+    [string]
+    $branch = "master",
+
+    [Parameter(Mandatory = $False)]
+    [switch]
+    $restart,
+    
+    [Parameter(Mandatory = $False)]
+    [string]
+    $additionalPreScript = "",
+    
+    [Parameter(Mandatory = $False)]
+    [string]
+    $additionalPostScript = "",
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $name,
+
+    [Parameter(Mandatory = $False)]
+    [string]
+    $authToken = $null,
+
+    [Parameter(Mandatory = $False)]
+    [string]
+    $debugScripts
+)
+
+if ($debugScripts -eq "true") {
+    New-Item -ItemType File -Path "c:\enableDebugging"
+    $DebugPreference = "Continue"
+}
+
+if (-not $restart) {
+    # Handle additional script
+    if ($additionalPreScript -ne "") {
+        [DownloadWithRetry]::DoDownloadWithRetry($additionalPreScript, 5, 10, $authToken, 'c:\scripts\additionalPreScript.ps1', $false)
+        & 'c:\scripts\additionalPreScript.ps1' -branch "$branch" -authToken "$authToken"
+    }
+}
+else {
+    # Handle additional script
+    if ($additionalPreScript -ne "") {
+        & 'c:\scripts\additionalPreScript.ps1' -branch "$branch" -authToken "$authToken" -restart 
+    }
+}
+
+if (-not $restart) {
+    # Setup profile
+    Write-Debug "Download profile file"
+    [DownloadWithRetry]::DoDownloadWithRetry("https://raw.githubusercontent.com/DennisVermeulen/windows_lab_setup/main/scripts/profile.ps1", 5, 10, $null, $PROFILE.AllUsersAllHosts, $false)
+    
+    # SSH and Choco setup
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    choco feature enable -n allowGlobalConfirmation
+    choco install --no-progress --limit-output vscode
+    choco install --no-progress --limit-output googlechrome
+    choco install --no-progress --limit-output vagrant
+    choco install --no-progress --limit-output virtualbox
+    choco install --no-progress --limit-output docker-desktop
+
+    Write-Host "try to get access token"
+    $content = [DownloadWithRetry]::DoDownloadWithRetry('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net', 5, 10, $null, $null, $true) | ConvertFrom-Json
+    $KeyVaultToken = $content.access_token
+    $tries = 1
+    while ($tries -le 10) { 
+        try {
+            Write-Host "download SSH key"
+            $secretJson = [DownloadWithRetry]::DoDownloadWithRetry("https://$name-vault.vault.azure.net/secrets/sshPubKey?api-version=2016-10-01", 5, 10, "Bearer $KeyVaultToken", $null, $false) | ConvertFrom-Json
+            
+            $secretJson.value | Out-File 'c:\ProgramData\ssh\administrators_authorized_keys' -Encoding utf8
+
+            ### adapted (pretty much copied) from https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/openssh/tools/chocolateyinstall.ps1#L433
+            $path = "c:\ProgramData\ssh\administrators_authorized_keys"
+            $acl = Get-Acl -Path $path
+            # following SDDL implies 
+            # - owner - built in Administrators
+            # - disabled inheritance
+            # - Full access to System
+            # - Full access to built in Administrators
+            $acl.SetSecurityDescriptorSddlForm("O:BAD:PAI(A;OICI;FA;;;SY)(A;OICI;FA;;;BA)")
+            Set-Acl -Path $path -AclObject $acl
+            ### end of copy
+
+            $tries = 11
+        }
+        catch {
+            Write-Host "Vault maybe not there yet, could still be deploying (try $tries)"
+            Write-Host $_.Exception
+            $tries = $tries + 1
+            Start-Sleep -Seconds 30
+        }
+    }
+
+    New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+    Restart-Service sshd
+
+    # Handle additional script
+    if ($additionalPostScript -ne "") {
+        [DownloadWithRetry]::DoDownloadWithRetry($additionalPostScript, 5, 10, $authToken, 'c:\scripts\additionalPostScript.ps1', $false)
+        & 'c:\scripts\additionalPostScript.ps1' -branch "$branch" -authToken "$authToken"
+    }
+}
+else {
+    # Handle additional script
+    if ($additionalPostScript -ne "") {
+        & 'c:\scripts\additionalPostScript.ps1' -branch "$branch" -authToken "$authToken" -restart 
+    }
+}
+
+class DownloadWithRetry {
+    static [string] DoDownloadWithRetry([string] $uri, [int] $maxRetries, [int] $retryWaitInSeconds, [string] $authToken, [string] $outFile, [bool] $metadata) {
+        $retryCount = 0
+        $headers = @{}
+        if (-not ([string]::IsNullOrEmpty($authToken))) {
+            $headers = @{
+                'Authorization' = $authToken
+            }
+        }
+        if ($metadata) {
+            $headers.Add('Metadata', 'true')
+        }
+
+        while ($retryCount -le $maxRetries) {
+            try {
+                if ($headers.Count -ne 0) {
+                    if ([string]::IsNullOrEmpty($outFile)) {
+                        $result = Invoke-WebRequest -Uri $uri -Headers $headers -UseBasicParsing
+                        return $result.Content
+                    }
+                    else {
+                        $result = Invoke-WebRequest -Uri $uri -Headers $headers -UseBasicParsing -OutFile $outFile
+                        return ""
+                    }
+                }
+                else {
+                    throw;
+                }
+            }
+            catch {
+                if ($headers.Count -ne 0) {
+                    write-host "download of $uri failed"
+                }
+                try {
+                    if ([string]::IsNullOrEmpty($outFile)) {
+                        $result = Invoke-WebRequest -Uri $uri -UseBasicParsing
+                        return $result.Content
+                    }
+                    else {
+                        $result = Invoke-WebRequest -Uri $uri -UseBasicParsing -OutFile $outFile
+                        return ""
+                    }
+                }
+                catch {
+                    write-host "download of $uri failed"
+                    $retryCount++;
+                    if ($retryCount -le $maxRetries) {
+                        Start-Sleep -Seconds $retryWaitInSeconds
+                    }            
+                }
+            }
+        }
+        return ""
+    }
+}
---
+++ windows10_course.tf
@@ -0,0 +1,83 @@
+
+ resource "azurerm_resource_group" "rg" {
+   name     = "rg-dv-mosadex"
+   location = "westeurope"
+ }
+ resource "azurerm_virtual_network" "rg" {
+   name                = "rg-mosadex-cursus"
+   address_space       = ["10.0.0.0/16"]
+   location            = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+ }
+ resource "azurerm_subnet" "rg" {
+   name                 = "internal"
+   resource_group_name  = azurerm_resource_group.rg.name
+   virtual_network_name = azurerm_virtual_network.rg.name
+   address_prefixes     = ["10.0.2.0/24"]
+ }
+ resource "azurerm_public_ip" "rg" {
+  count               = 1  
+  name                = "mosadex-publicip-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+ resource "azurerm_network_interface" "rg"{
+   count               = 1  
+   name                = "az-mosadex-nic-${count.index}"
+   location            = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+ ip_configuration {
+     name                          = "internal"
+     subnet_id                     = azurerm_subnet.rg.id
+     private_ip_address_allocation = "Dynamic"
+     public_ip_address_id          = azurerm_public_ip.rg["${count.index}"].id
+   }
+ }
+ resource "azurerm_windows_virtual_machine" "virtualmachine" {
+   count               = 1  
+   name                = "az-mosadex-${count.index}"
+   resource_group_name = azurerm_resource_group.rg.name
+   location            = azurerm_resource_group.rg.location
+   size                = "Standard_D8s_v3"
+   admin_username      = "mosadex"
+   admin_password      = "NPENA%+bb%Zp*v8c"
+   network_interface_ids = [
+     azurerm_network_interface.rg.*.id[count.index],
+   ]
+ os_disk {
+     caching              = "ReadWrite"
+     storage_account_type = "Standard_LRS"
+   }
+ source_image_reference {
+     publisher = "MicrosoftWindowsDesktop"
+     offer     = "Windows-10"
+     sku       = "win10-21h2-entn-ltsc-g2"
+     version   = "19044.1586.220303"
+   }  
+ }
+
+resource "azurerm_virtual_machine_extension" "initvirtualmachine" {
+  count               = 1  
+  name                       = "initvirtualmachine"
+  virtual_machine_id         = azurerm_windows_virtual_machine.virtualmachine["${count.index}"].id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "fileUris": [
+        "https://raw.githubusercontent.com/DennisVermeulen/windows_lab_setup/main/scripts/installs.ps1"
+      ]
+    }
+  SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File installs.ps1"
+    }
+  PROTECTED_SETTINGS
+
+}
